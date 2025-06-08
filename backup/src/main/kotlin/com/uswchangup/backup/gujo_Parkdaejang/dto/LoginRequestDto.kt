b2bb08bc1b3fc7613f9736b6e59e7c3472485b7a
ryou0914 <toryou209@gmail.com>
2025-06-08 14:10:35 +0900

    Delete backup directory

--- backup/src/main/kotlin/com/uswchangup/backup/gujo_Parkdaejang/dto/LoginRequestDto.kt
+++
@@ -1,6 +0,0 @@
-package com.uswchangup.backup.gujo_Parkdaejang.dto
-
-data class LoginRequestDto (
-    val username: String,
-    val password: String
-)
--- backup/src/main/kotlin/com/uswchangup/backup/gujo_Parkdaejang/repository/PhotoRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.uswchangup.backup.gujo_Parkdaejang.repository
-
-import com.uswchangup.backup.gujo_Parkdaejang.entity.PhotoEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PhotoRepository : JpaRepository<PhotoEntity, Long>
